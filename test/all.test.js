1de95fbcd4738a2ccca9deffbaf1627b34b552d0
lilahaj <fedor.pilotov@gmail.com>
2026-03-26 21:07:33 +0200

    tehtävä

---
+++ test/all.test.js
@@ -0,0 +1,94 @@
+import { expect } from "chai";
+
+import add from "../src/add.js";
+import at from "../src/at.js";
+import camelCase from "../src/camelCase.js";
+import capitalize from "../src/capitalize.js";
+import castArray from "../src/castArray.js";
+import ceil from "../src/ceil.js";
+import clamp from "../src/clamp.js";
+import divide from "../src/divide.js";
+import drop from "../src/drop.js";
+import filter from "../src/filter.js";
+import get from "../src/get.js";
+import isEmpty from "../src/isEmpty.js";
+import reduce from "../src/reduce.js";
+import toString from "../src/toString.js";
+import words from "../src/words.js";
+
+describe("Simple coverage tests", () => {
+  it("add should add two numbers", () => {
+    expect(add(2, 3)).to.equal(5);
+  });
+
+  it("at should return values at given paths", () => {
+    const object = { a: [{ b: { c: 3 } }, 4] };
+    expect(at(object, ["a[0].b.c", "a[1]"])).to.deep.equal([3, 4]);
+  });
+
+  it("camelCase should convert to camel case", () => {
+    expect(camelCase("Foo Bar")).to.be.a("string");
+  });
+
+  it("capitalize should capitalize the first letter", () => {
+    expect(capitalize("FRED")).to.equal("Fred");
+  });
+
+  it("castArray should make an array", () => {
+    expect(castArray(1)).to.deep.equal([1]);
+  });
+
+  it("ceil should round up", () => {
+    expect(ceil(4.006)).to.equal(5);
+  });
+
+  it("clamp should restrict a number", () => {
+    expect(clamp(-10, -5, 5)).to.be.a("number");
+  });
+
+  it("divide should divide two numbers", () => {
+    expect(divide(6, 4)).to.be.a("number");
+  });
+
+  it("drop should drop elements", () => {
+    expect(drop([1, 2, 3], 2)).to.deep.equal([3]);
+  });
+
+  it("filter should filter elements", () => {
+    const users = [
+      { user: "barney", active: true },
+      { user: "fred", active: false },
+    ];
+    const res = filter(users, ({ active }) => active);
+    expect(res.length).to.equal(1);
+    expect(res[0].user).to.equal("barney");
+  });
+
+  it("get should retrieve value", () => {
+    const object = { a: [{ b: { c: 3 } }] };
+    expect(get(object, "a[0].b.c")).to.equal(3);
+  });
+
+  it("isEmpty should check if empty", () => {
+    expect(isEmpty(null)).to.be.true;
+    expect(isEmpty(true)).to.be.true;
+    expect(isEmpty([1, 2, 3])).to.be.false;
+  });
+
+  it("reduce should accumulate values", () => {
+    expect(reduce([1, 2], (sum, n) => sum + n, 0)).to.equal(3);
+  });
+
+  it("toString should convert to string", () => {
+    expect(toString(null)).to.be.a("string");
+    expect(toString(1)).to.be.a("string");
+  });
+
+  it("words should split to words", () => {
+    expect(words("fred, barney, & pebbles")).to.deep.equal([
+      "fred",
+      "barney",
+      "pebbles",
+    ]);
+  });
+});
---
+++ test/more.test.js
@@ -0,0 +1,121 @@
+import { expect } from "chai";
+
+import countBy from "../src/countBy.js";
+import defaultTo from "../src/defaultTo.js";
+import difference from "../src/difference.js";
+import endsWith from "../src/endsWith.js";
+import eq from "../src/eq.js";
+import every from "../src/every.js";
+import isArguments from "../src/isArguments.js";
+import isBoolean from "../src/isBoolean.js";
+import isDate from "../src/isDate.js";
+import isLength from "../src/isLength.js";
+import isObject from "../src/isObject.js";
+import isSymbol from "../src/isSymbol.js";
+import isTypedArray from "../src/isTypedArray.js";
+import keys from "../src/keys.js";
+import map from "../src/map.js";
+import slice from "../src/slice.js";
+
+describe("More simple coverage tests", () => {
+  it("countBy counts elements", () => {
+    const users = [
+      { user: "barney", active: true },
+      { user: "betty", active: true },
+      { user: "fred", active: false },
+    ];
+    expect(countBy(users, (value) => value.active)).to.be.an("object");
+  });
+
+  it("defaultTo returns default value for null/undefined/NaN", () => {
+    expect(defaultTo(1, 10)).to.be.a("number");
+    expect(defaultTo(undefined, 10)).to.be.a("number");
+    expect(defaultTo(NaN, 10)).to.be.a("number");
+  });
+
+  it("difference returns difference of arrays", () => {
+    expect(difference([2, 1], [2, 3])).to.deep.equal([1]);
+  });
+
+  it("endsWith checks end of string", () => {
+    expect(endsWith("abc", "c")).to.be.true;
+    expect(endsWith("abc", "b")).to.be.false;
+  });
+
+  it("eq compares values", () => {
+    const object = { a: 1 };
+    expect(eq(object, object)).to.be.true;
+    expect(eq(object, { a: 1 })).to.be.false;
+    expect(eq("a", "a")).to.be.true;
+  });
+
+  it("every checks if all elements pass", () => {
+    expect(every([true, 1, null, "yes"], Boolean)).to.be.false;
+    expect(every([true, 1, "yes"], Boolean)).to.be.true;
+  });
+
+  it("isArguments checks if value is arguments", () => {
+    expect(
+      isArguments(
+        (function () {
+          return arguments;
+        })(),
+      ),
+    ).to.be.true;
+    expect(isArguments([1, 2, 3])).to.be.false;
+  });
+
+  it("isBoolean checks if value is boolean", () => {
+    expect(isBoolean(false)).to.be.true;
+    expect(isBoolean(null)).to.be.false;
+  });
+
+  it("isDate checks if value is date", () => {
+    expect(isDate(new Date())).to.be.true;
+    expect(isDate("Mon April 23 2012")).to.be.false;
+  });
+
+  it("isLength checks if value is valid length", () => {
+    expect(isLength(3)).to.be.true;
+    expect(isLength(Number.MIN_VALUE)).to.be.false;
+    expect(isLength(Infinity)).to.be.false;
+  });
+
+  it("isObject checks if value is object", () => {
+    expect(isObject({})).to.be.true;
+    expect(isObject([1, 2, 3])).to.be.true;
+    expect(isObject(null)).to.be.false;
+  });
+
+  it("isSymbol checks if value is symbol", () => {
+    expect(isSymbol(Symbol.iterator)).to.be.true;
+    expect(isSymbol("abc")).to.be.false;
+  });
+
+  it("isTypedArray checks if value is typed array", () => {
+    expect(isTypedArray(new Uint8Array())).to.be.true;
+    expect(isTypedArray([])).to.be.false;
+  });
+
+  it("keys returns keys of object", () => {
+    function Foo() {
+      this.a = 1;
+      this.b = 2;
+    }
+    Foo.prototype.c = 3;
+    expect(keys(new Foo())).to.deep.equal(["a", "b"]);
+    expect(keys("hi")).to.deep.equal(["0", "1"]);
+  });
+
+  it("map iterates and maps array", () => {
+    function square(n) {
+      return n * n;
+    }
+    expect(map([4, 8], square)).to.deep.equal([16, 64]);
+  });
+
+  it("slice creates slice of array", () => {
+    const array = [1, 2, 3, 4];
+    expect(slice(array, 1, 3)).to.deep.equal([2, 3]);
+  });
+});
